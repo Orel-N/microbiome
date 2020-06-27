f4410abeea4d6371155e4d29e5d9283386ea4f0b
Orel-N <neza.orel@nib.si>
2020-06-27 18:39:33 +0200

    first commit

--- phyloseq_2015.R
+++ phyloseq_2015.R
@@ -6,25 +6,124 @@
 #Loading packages
 library(phyloseq); packageVersion("phyloseq")
 library(ggplot2); packageVersion("ggplot2")
-
+if (packageVersion("devtools") < 1.6) {
+  install.packages("devtools")
+}
+devtools::install_github("hadley/lazyeval")
+devtools::install_github("hadley/dplyr")
+library(dplyr)
+library(rstatix); packageVersion("rstatix")
+library(ggpubr); packageVersion("ggpubr")
+library(vegan); packageVersion("vegan")
 theme_set(theme_bw())
 
-###Import dada2 output into phyloseq
-#metadata
-meta <- read.table("C:/Users/nezao/Documents/1-MIKROBIOM/1-Eksperimentalno delo/2. Eksperiment - TEREN/Rezultati/BacterialCommunity/2015/metadata.txt", row.names = 1)
+##################################
+#Import dada2 output into phyloseq
+##################################
+
+#Import Sample Data - "metadata"(data.frame)
+meta <- read.table("./data/metadata.txt", row.names = 1)
 meta
 
+#Import "ASV table" (matrix)
+seqtab.nochim <- readRDS ("./data/seqtab.nochim.rds")
+#correct names
+#colnames(seqtab.nochim) <- paste0("ASV", 1:ncol(seqtab.nochim))
 
-#Create a phyloseq object from the ASV table and taxonomyassigned by DADA2
+#Import taxonomy table (matrix)
+taxa <- readRDS ("./data/taxa.rds")
+
+#Check order
+all.equal(colnames(seqtab.nochim), rownames(taxa))
+
+#Create a phyloseq object from the OTU table/ASV table and taxonomy assigned by DADA2
 ps <-phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), sample_data(meta), tax_table(taxa))
 ps
 
-row.names(seqtab.nochim)
-sample_names(seqtab.nochim)
+#add reference sequence and replace variants with ASVs
+ps2 <- Biostrings::DNAStringSet(taxa_names(ps))
+names(ps2) <- taxa_names(ps)
+ps <- merge_phyloseq(ps, ps2)
+taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps)))
+ps
+
+
+##################################
+#Generate an overview table
+##################################
+
+summary_table <- data.frame()
+
+#metadata
+meta <- as(sample_data(ps), "data.frame")
+meta$Row.names<-paste("X",row.names(meta), sep ="")
+
+#summary of dada2 workflow
+track <- readRDS ("./data/track.rds")
+reads.tab <- as.data.frame(track)
+reads.tab$Row.names <-paste("X",row.names(reads.tab), sep ="")
+
+#check how many ASVs were unclassified on phylum level, or assigned to chloroplast and Mitochondria
+ps_euk <- as(sample_sums(subset_taxa(ps, Kingdom %in% c("Eukaryota"))),"vector")
+ps_uncl <- as(sample_sums(subset_taxa(ps, Phylum %in% c("Bacteria_uncl","Archaea_uncl","NA_uncl"))),"vector")
+ps_chl <- as(sample_sums(subset_taxa(ps, Order %in% c("Chloroplast"))),"vector")
+ps_mit <- as(sample_sums(subset_taxa(ps, Family %in% c("Mitochondria"))),"vector")
+
+ps_chl
+ps_mit
+
+pruned_seq_sums <- data.frame(Chloroplast = ps_chl,Mitochondria = ps_mit)
+pruned_seq_sums$Row.names <- paste("X",row.names(pruned_seq_sums), sep ="")
+
+#remove unclassified on phylum level, chloroplast and Mitochondrial sequence variants
+phy_obj0 <- subset_taxa(ps, !Kingdom %in% c("Eukaryota") &!Phylum %in% c("Bacteria_uncl","Archaea_uncl","NA_uncl") & !Order %in% c("Chloroplast") & !Family %in% c("Mitochondria") )
+
+#alpha diversity indeces
+ps0_alpha <- estimate_richness(phy_obj0, measures = c("Observed", "Chao1","Shannon", "InvSimpson"))
+ps0_alpha$Row.names<-rownames(ps0_alpha)
+
+#merge all together
+ps0_summary_table <- merge(meta,reads.tab,by ="Row.names") %>%
+  merge(pruned_seq_sums,by ="Row.names")%>%
+  merge(ps0_alpha,by ="Row.names")%>%
+  mutate_if(is.numeric, round, 2) %>%
+  mutate(Seq.prop = round(nonchim/input,2),
+         Chloroplast.Seq.prop = round(Chloroplast/nonchim,2),
+         Mitochondria.Seq.prop = round(Mitochondria/nonchim,2)) %>%
+  select("Location","Season", #metadata
+         "input","filtered", "merged", "nonchim", #dada2 
+         "Chloroplast","Mitochondria", #taxa
+         "Observed","Chao1","Shannon","InvSimpson") #alpha div
+
+summary_table <- rbind(summary_table,ps0_summary_table)
+
+write.table(summary_table, "./Microbiome_overview_table.txt" , sep = "\t", quote = F)
+
+#Statistical comparison by type of samples
+
+Chao1_Wilcox <- summary_table   %>%
+  group_by(Season) %>%
+  rstatix::wilcox_test(Chao1 ~ Location, p.adjust.method = "BH",paired = TRUE) %>%
+  add_significance()
+
+Shannon_Wilcox <- summary_table   %>%
+  group_by(Season) %>%
+  rstatix::wilcox_test(Shannon ~ Location, p.adjust.method = "BH",paired = TRUE) %>%
+  add_significance()
+
+InvSimpson_Wilcox <- summary_table   %>%
+  group_by(Season) %>%
+  rstatix::wilcox_test(InvSimpson ~ Location, p.adjust.method = "BH",paired = TRUE) %>%
+  add_significance()
+
+#####################################
+#Plot rarefaction
+####################################
+
 
 
 #Alpha diversity
-plot_richness(ps, x="Season", measures=c("Shannon", "Simpson"), color="Location")
+plot_richness(phy_obj0, x="Season", measures=c("Shannon", "Simpson"), color="Location")
 ggsave("AlphaDiversity_seasons.pdf", last_plot(), device = "pdf")
 
 plot_richness(ps, x="Location", measures=c("Shannon", "Simpson"), color="Season")
