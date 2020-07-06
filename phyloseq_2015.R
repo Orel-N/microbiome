61a92b59732026f81fb4854c595c743df8863ca8
Orel-N <neza.orel@nib.si>
2020-07-06 15:58:02 +0200

    Changed

--- phyloseq_2015.R
+++ phyloseq_2015.R
@@ -98,22 +98,6 @@ summary_table <- merge(meta,reads.tab,by ="Row.names") %>%
 write.table(summary_table, "./Micro_overview_table.txt" , sep = "\t", quote = F)
 
 
-#Statistical comparison by type of samples
-
-Chao1_Wilcox <- summary_table   %>%
-  group_by(Season) %>%
-  rstatix::wilcox_test(Chao1 ~ Location, p.adjust.method = "BH",paired = TRUE) %>%
-  add_significance()
-
-Shannon_Wilcox <- summary_table   %>%
-  group_by(Season) %>%
-  rstatix::wilcox_test(Shannon ~ Location, p.adjust.method = "BH",paired = TRUE) %>%
-  add_significance()
-
-InvSimpson_Wilcox <- summary_table   %>%
-  group_by(Season) %>%
-  rstatix::wilcox_test(InvSimpson ~ Location, p.adjust.method = "BH",paired = TRUE) %>%
-  add_significance()
 
 #####################################
 #Plot rarefaction
@@ -121,131 +105,9 @@ InvSimpson_Wilcox <- summary_table   %>%
 
 phy_obj <- phy_obj0
 
+#https://cran.r-project.org/web/packages/iNEXT/vignettes/Introduction.html
+
 ps0.iNEXT <- iNEXT(as.data.frame(otu_table(phy_obj)), q=0, datatype="abundance", knots = 40)
 
-ps0.iNEXT.rare <-fortify(ps0.iNEXT, type=1)
-ps0.iNEXT.meta <- as(sample_data(phy_obj), "data.frame")
-ps0.iNEXT.meta$site <- rownames(ps0.iNEXT.meta)
-
-ps0.iNEXT.rare$Location <- ps0.iNEXT.meta$Location[match(ps0.iNEXT.rare$site, ps0.iNEXT.meta$site)] 
-ps0.iNEXT.rare$SampleName <- ps0.iNEXT.meta$sample_title[match(ps0.iNEXT.rare$site, ps0.iNEXT.meta$site)] 
-
-ps0.iNEXT.rare.point <- ps0.iNEXT.rare[which(ps0.iNEXT.rare$method == "observed"),]
-ps0.iNEXT.rare.line <- ps0.iNEXT.rare[which(ps0.iNEXT.rare$method != "observed"),]
-ps0.iNEXT.rare.line$method <- factor (ps0.iNEXT.rare.line$method,
-                                      c("interpolated", "extrapolated"),
-                                      c("interpolation", "extrapolation"))
-
-ps0.iNEXT.rare.line$Primer <- p
-ps0.iNEXT.rare.point$Primer <- p
-
-iNEXT.rare.line <- rbind(iNEXT.rare.line,ps0.iNEXT.rare.line)
-iNEXT.rare.point <- rbind(iNEXT.rare.point,ps0.iNEXT.rare.point)
-
-
-
-iNEXT.rare.line$Type <- factor(iNEXT.rare.line$Type, levels = c("Sea ice", "Surface water", "Deep water", "Sediment trap","Sediment"))
-iNEXT.rare.point$Type <- factor(iNEXT.rare.point$Type, levels = c("Sea ice", "Surface water", "Deep water", "Sediment trap","Sediment"))
-
-rare.p <- ggplot(iNEXT.rare.line, aes(x=x, y=y, shape = site))+
-  geom_line(aes(linetype = method, colour = Primer), lwd = 0.5, data= iNEXT.rare.line)+
-  geom_point(shape = 21, size =3, colour = "black", data= iNEXT.rare.point)+
-  labs(x = "Sample size", y = "Species richness")+
-  xlim(0,3e5)+
-  theme_classic(base_size = 12)+theme(legend.position="none")+
-  facet_grid(Type~Primer, scales = "free",as.table = TRUE)+
-  geom_hline(aes(yintercept=-Inf)) + 
-  geom_vline(aes(xintercept=-Inf)) + 
-  coord_cartesian(clip="off")
-
-
-ggsave("Figures/rarefactions.pdf", rare.p)
-###
-
-################################################
-#Alpha diversity
-plot_richness(phy_obj0, x="Season", measures=c("Shannon", "Simpson"), color="Location")
-ggsave("AlphaDiversity_seasons.pdf", last_plot(), device = "pdf")
-
-plot_richness(ps, x="Location", measures=c("Shannon", "Simpson"), color="Season")
-ggsave("AlphaDiversity_location.pdf", last_plot(), device = "pdf")
-
-# Transform data to proportions as appropriate for Bray-Curtis distances
-ps.prop <- transform_sample_counts(ps, function(otu) otu/sum(otu))
-ord.nmds.bray <- ordinate(ps.prop, method="NMDS", distance="bray")
-
-p <- plot_ordination(ps.prop, ord.nmds.bray, color="Season", shape="Location", title="Bray NMDS")+ geom_point(size=4)
-plot(p)
-ggsave("NMDS_Seasons2.pdf", last_plot())
-
-
-
-plot_ordination(ps.prop, ord.nmds.bray, color="Season", title="Bray NMDS")
-ggsave("NMDS_Location.pdf", last_plot())
-
-#Barplot
-top20 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:20]
-ps.top20 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
-ps.top20 <- prune_taxa(top20, ps.top20)
-plot_bar(ps.top20, x="Season", fill="Family") + facet_wrap(~Location, scales="free_x")
-
-top40 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:40]
-ps.top40 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
-ps.top40 <- prune_taxa(top40, ps.top40)
-plot_bar(ps.top40, x="Season", fill="Family") + facet_wrap(~Location, scales="free_x")
-
-
-
-plot_bar(ps.top30, x="Season", fill="Family") + facet_wrap(~Location, scales="free_x")
-
-#Marge ASV at the class level
-#NArm set FALSE forces the function to keep the unclassified OTU at the class level
-ps.class = tax_glom(ps, taxrank="Class", NArm=FALSE)
-ps.class
-plot_bar(ps.class, fill="Class", x="Location")+facet_wrap(~Season, scales="free_x", nrow=1)
-ps.order = tax_glom(ps, taxrank="Order", NArm=FALSE)
-ps.order
-plot_bar(ps.order, fill="Order", x="Location")+facet_wrap(~Season, scales="free_x", nrow=1)
-top50.class <- names(sort(taxa_sums(ps.order), decreasing=TRUE))[1:50]
-ps.top50.class <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
-ps.top50.class <- prune_taxa(top50.class, ps.top50.class)
-plot_bar(ps.top50.class, x="Location", fill="Order") + facet_wrap(~Season, scales="free_x", nrow=1)
-
-
-#Top 1000
-top1000 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:1000]
-ps.top1000 <- prune_taxa(top1000, ps)
-ps.top1000.class = tax_glom(ps.top1000, taxrank="Class", NArm=FALSE)
-ps.top1000.class
-plot_heatmap(ps.class, "NMDS", "bray", "Location", "Class", low="#FFFFCC", high="#000033", na.value="white")+facet_wrap(~Season, scales="free_x", nrow=1)
-
-
-plot_bar(ps.class, fill="Class", x="Location")+facet_wrap(~Season, scales="free_x", nrow=1)
-ps.order = tax_glom(ps, taxrank="Order", NArm=FALSE)
-ps.order
-
-#HeatMap
-gpt <- subset_taxa(ps, Kingdom=="Bacteria")
-gpt <- prune_taxa(names(sort(taxa_sums(gpt),TRUE)[1:30]), gpt)
-plot_heatmap(gpt, sample.label="Location")
-
-p <- plot_heatmap(gpt, "NMDS", "bray", "Location", "Family", low="#FFFFCC", high="#000033", na.value="white")
-p
-
-p.class <- plot_heatmap(ps.top50.class, "NMDS", "bray", "Location", "Class", low="#FFFFCC", high="#000033", na.value="white")
-p.class
-
-
-#OTU that represent at least 20% of reads in at least one sample
-ps.top <- filter_taxa(ps, function(x) sum(x > total*0.20) > 0, TRUE)
-ps.top
-
-
-
-#info
-nsamples(ps)
-ntaxa(ps)
-#total count in each sample
-head(sample_sums(ps))
-rank_names(ps)
+ggiNEXT(ps0.iNEXT, type = 1)
 
