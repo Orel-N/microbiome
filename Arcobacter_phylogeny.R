712323818c56bc6ac39ae3acf859dd7b6a8ab315
Orel-N <neza.orel@nib.si>
2020-11-10 12:10:28 +0100

    New commit

--- Arcobacter_phylogeny.R
+++ Arcobacter_phylogeny.R
@@ -1,20 +1,18 @@
+##############################################
+#Arcobacter phylogenetic tree
+##############################################
 
+#Load packages
 library(dplyr)
 library(ggplot2)
 library(phyloseq)
-library(ggpubr)
-library(RColorBrewer)
-library(iNEXT)
-library(DESeq2)
-library(rstatix)
-library(vegan)
 library(Biostrings)
 library("ape")
 library("ggtree") 
-library(phyloseq)
-library(ggjoy)
+library(tidyr)
 
 
+#Load data
 phy_obj3 <- readRDS("./phyloseqFiltered.RDS")
 
 phy_obj3 <- subset_samples(phy_obj3, Dataset == "2020")
@@ -24,20 +22,24 @@ phy_obj3
 phy_obj3.ra <- transform_sample_counts(phy_obj3, function(x) x / sum(x))
 
 
-##############################
-#Subset taxa
-##############################
+##################################
+#Subset taxa and extract seqences
+##################################
 
-Campylobacteria_class <- subset_taxa(phy_obj3, Class == "Campylobacteria")
-Arcobacter_family <- subset_taxa(phy_obj3, Family == "Arcobacteraceae")
-Vibrio_family <- subset_taxa(phy_obj3, Family == "Vibrionaceae")
-Mycobacteriaceae_family <- subset_taxa(phy_obj3, Family == "Mycobacteriaceae")
-
-Campylobacteria_class.rs <- refseq(Campylobacteria_class)
-writeXStringSet(Campylobacteria_class.rs, "./Campylobacteria_class.fasta", format = "fasta")
+#Arcobacter family
+Arcobacter_family <- subset_taxa(phy_obj3.ra, Family == "Arcobacteraceae")
 
 Arcobacter_family.rs <- refseq(Arcobacter_family)
 writeXStringSet(Arcobacter_family.rs, "./Arcobacter_family.fasta", format = "fasta")
+
+#Similar for other groups
+
+Campylobacteria_class <- subset_taxa(phy_obj3.ra, Class == "Campylobacteria")
+Vibrio_family <- subset_taxa(phy_obj3.ra, Family == "Vibrionaceae")
+Mycobacteriaceae_family <- subset_taxa(phy_obj3.ra, Family == "Mycobacteriaceae")
+
+Campylobacteria_class.rs <- refseq(Campylobacteria_class)
+writeXStringSet(Campylobacteria_class.rs, "./Campylobacteria_class.fasta", format = "fasta")
 
 Vibrio_family.rs <- refseq(Vibrio_family)
 writeXStringSet(Vibrio_family.rs, "./Vibrio_family.fasta", format = "fasta")
@@ -46,64 +48,123 @@ Mycobacteriaceae_family.rs <- refseq(Mycobacteriaceae_family)
 writeXStringSet(Mycobacteriaceae_family.rs, "./Mycobacteriaceae_family.fasta", format = "fasta")
 
 Mic_Ind <- read.table("./data/Microbial_Indicators.txt", h=T, sep="\t")
-ps_Mic <- subset_taxa(phy_obj3, Family %in% c(Mic_Ind$Family))
-ps.ra_Mic <- subset_taxa(phy_obj3ra, Family %in% c(Mic_Ind$Family))
+ps_Mic <- subset_taxa(phy_obj3.ra, Family %in% c(Mic_Ind$Family))
 
 
-##############################
-#Silva tree with closest relatives
-##############################
+###################################
+#Phylogenetic tree with closest relatives (SILVA platform)
+###################################
 
+#Upload Arcobacter_family.fasta on Silva webside and create tree 
+#(Classify 0.97 similarity, compute tree: Denovo including neighbours, program:RAxML, model: GTR)
+#View in wasabi and download results
+
+#Import results in R
 filename <- "./data/Arcobacter.nex"
 t1 <- ape::read.nexus(filename)
 t1
 plot_tree(t1, label.tips="taxa_names")
+
 ggtree(t1, layout = "circular") + geom_tiplab()
 ggtree(t1) + geom_tiplab(as_ylab=TRUE, hjust = -.2, color='firebrick')
 
+#Problem: 
+#Show names of closest neighbours
+#Some closest neighbours are "uncultured bacterium", "uncultured Epsilonproteobacteria"..
 
-##############################
+
+############################################
 #Create phylogenetic tree of phyloseq object
-##############################
+###########################################
 
+###Create tree for Arcobacter phyloseq
 
-random_tree = rtree(ntaxa(phy_obj3ra), rooted=TRUE, tip.label=taxa_names(phy_obj3ra))
-phy_obj3ra.2 = merge_phyloseq(phy_obj3ra, random_tree)
+random_tree = rtree(ntaxa(phy_obj3.ra), rooted=TRUE, tip.label=taxa_names(phy_obj3.ra))
+phy_obj3ra.2 = merge_phyloseq(phy_obj3.ra, random_tree)
 
 Arcobacter_family_2 <- subset_taxa(phy_obj3ra.2, Family == "Arcobacteraceae")
+Arcobacter_family_2
+
 plot_tree(Arcobacter_family_2, label.tips="taxa_names", color = "Location",
           size="abundance", ladderize = "left")
 
-
-Arcobacter_family_2
-
 ggtree(Arcobacter_family_2) + geom_tiplab() 
 
-#Create phylogenetic tree with reference seq
-library(ape)
-library(Biostrings)
+###Create phylogenetic tree for Arcobacter phyloseq with reference seq
 
-rs <- readDNAStringSet("./data/Arcobacter_ref_seq_NCBI.fasta", format = "fasta")
+##Create phyloseq with reference seq
+
+#Find reference seq for Arcobacter in studies: (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4224126/ 
+#and https://www.sciencedirect.com/science/article/pii/S0043135419311790 )
+#Download FASTA from NCBI
+#Help for the code: https://github.com/joey711/phyloseq/issues/1150 
+
+
+#Load FASTA of ref seq in R
+rs <- readDNAStringSet(file="./data/Arcobacter_ref_seq_NCBI.fasta", format = "fasta", 
+                       nrec = -1L, skip = 0L, seek.first.rec = FALSE, use.names = TRUE)
 seq_name = names(rs)
 sequence = paste(rs)
 df <- data.frame(seq_name, sequence)
 df
 
-otumat <- matrix(1, nrow = 1, ncol = length(rs))
+#Create otu_table
+otumat <- matrix(0.0001, nrow = 1, ncol = length(rs))
 colnames(otumat) <- names(rs)
 rownames(otumat) <- "Reference"
 OTU <- otu_table(otumat, taxa_are_rows = FALSE)
 
-SAM <- sample_data(Arcobacter_family_2)[1,]
+#Create metadata table
+SAM <- sample_data(Arcobacter_family)[1,]
 SAM[,] <- NA
 sample_names(SAM) <- "Reference"
 # or : rownames(SAM) <- "Reference"
-ps.ref <- phyloseq(OTU, SAM, rs)
+
+#Create taxonomy table
+
+tax_df <- data.frame(wholetax = rep(c("level1.level2.level3.level4.level5.level6"), 36))
+rownames(tax_df) <- names(rs)  
+tax_df <- tax_df %>% separate(wholetax, c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6"))
+ref_taxa <- tax_table(as.matrix(tax_df))
+ref_taxa
+
+#Create phyloseq
+ps.ref <- phyloseq(OTU, SAM, rs, ref_taxa)
 ps.ref
 
-ps.merged <- merge_phyloseq(Arcobacter_family_2, ps.ref)
+##Merge Arcobacter phyloseq and reference phyloseq
+
+ps.merged <- merge_phyloseq(Arcobacter_family, ps.ref)
 
 ps.merged
-Arcobacter_family_2
+Arcobacter_family
 ps.ref
+
+##Calculate phylogenetic tree
+
+#load libraries
+library(DECIPHER)
+library(phangorn)
+
+# calculate tree
+seqs <- refseq(ps.merged)
+alignment <- AlignSeqs(DNAStringSet(seqs), anchor = NA)
+phang.align <- as.phyDat(as(alignment, "matrix"), type = "DNA")
+dm <- dist.ml(phang.align)
+treeNJ <- NJ(dm) # Note, tip order != sequence order
+fit = pml(treeNJ, data = phang.align)
+fitGTR <- update(fit, k = 4, inv = 0.2)
+fitGTR <- optim.pml(fitGTR, model = "GTR", optInv = TRUE, optGamma = TRUE,
+                    rearrangement = "stochastic", control = pml.control(trace = 0))
+detach("package:phangorn", unload = TRUE)
+
+# merge ps object with tree
+ps.merged <- merge_phyloseq(ps.merged, phy_tree(fitGTR$tree))
+ps.merged
+
+##Plot tree
+plot_tree(ps.merged, label.tips="taxa_names", ladderize = "left")
+
+plot_tree(ps.merged, label.tips="taxa_names", color = "Location", size = "abundance", ladderize = "left")
+
 
