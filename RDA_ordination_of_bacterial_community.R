c2200bc0e4027a620a3cba3933b6ab08d5087969
Orel-N <neza.orel@nib.si>
2020-11-17 10:48:50 +0100

    New commit

--- RDA_ordination_of_bacterial_community.R
+++ RDA_ordination_of_bacterial_community.R
@@ -8,7 +8,6 @@
 #Set working directory
 setwd("C:/Users/nezao/Documents/5-R/Microbiome2015_2020")
 
-
 #Load packages
 library("ggplot2"); packageVersion("ggplot2")
 library(rstatix);packageVersion("rstatix")
@@ -17,6 +16,11 @@ library(tidyr);packageVersion("tidyr")
 library(tidyverse);packageVersion("tidyverse")
 library("vegan"); packageVersion("vegan")
 library(RColorBrewer)
+
+
+####################################
+#Import data
+####################################
 
 #Import Sample Data - "metadata"
 meta <- read.csv("./data/Metadata_2015_2020.csv", h=T, sep = ",")
@@ -36,9 +40,10 @@ Mic_Ind <- read.table("./data/Microbial_Indicators.txt", h=T, sep="\t")
 ps.ind.vst <- subset_taxa(phy_obj3, Family %in% c(Mic_Ind$Family))
 ps.ind.vst
 
-################################
-#Correlation between env. parameters
-################################
+
+###############################################################
+#Correlation between env. parameters - BEFORE FORWARD SELECTION
+###############################################################
 
 #define scale function
 scale_par <- function(x) scale(x, center = FALSE, scale = TRUE)[,1]
@@ -46,8 +51,6 @@ scale_par <- function(x) scale(x, center = FALSE, scale = TRUE)[,1]
 #import env.par. data
 metadata.raw <- all.data
 env.par <- c("Temperature_sea","Salinity", "Dissolved_Oxygen_perc", "DOC", "TDN", "NO2", "NO3", "PO4", "SiO3", "NH4")
-env.par <- c("Temperature_sea", "DOC", "Dissolved_Oxygen_perc", "NH4")
-#env.par <- c("Temperature_sea","Salinity","DOC", "DIN")
 
 #scale all parameters
 metadata.scaled.SRF <- metadata.raw %>% 
@@ -69,9 +72,9 @@ envpar_corr %>%
   cor_plot(label = TRUE)
 
 
-#####################################
-#RDA ordination of the fitted model of bacterial composition data constrained by environmental variables
-####################################
+###########################################
+#RDA ordination - BEFORE FORWARD SELECTION
+###########################################
 
 #subset by fraction and remove NAs in metadata
 phy_obj3
@@ -92,14 +95,99 @@ BAC_20.no.na <- phy_obj3 %>%
 BAC_20.no.na
 
 ##remove unobserved OTU and OTU from 2015 dataset
-BAC_20.no.na <- prune_taxa(taxa_sums(BAC_20.no.na)!=0,BAC_20.no.na)
-BAC_20.no.na
+#BAC_20.no.na <- prune_taxa(taxa_sums(BAC_20.no.na)!=0,BAC_20.no.na)
+#BAC_20.no.na
 
 #extract and scale the env. parameters
 BAC_20.env <- data.frame(sample_data(BAC_20.no.na))[c("Temperature_sea","Salinity", "Dissolved_Oxygen_perc", "DOC", "TDN", "NO2", "NO3", "PO4", "SiO3", "NH4")]  
+BAC_20.env <- as.data.frame(scale(BAC_20.env,center = FALSE, scale = TRUE))
+
+#extract OTU tables from Phyloseq object
+BAC_20.otu <- t(otu_table(BAC_20.no.na))
+
+#RDA analysis
+BAC_20.rda.all <- rda (BAC_20.otu ~ ., data = BAC_20.env) # model including all variables
+
+
+#####################################
+#Forward selection of explanatory variables
+#####################################
+
+#based on the tutorial from David Zeleny Lab
+#http://www.davidzeleny.net/anadat-r/doku.php/en:forward_sel
+
+BAC_20.rda.0 <- rda (BAC_20.otu ~ 1, data = BAC_20.env) # model containing only species matrix and intercept
+BAC_20.rda.sel.os <- ordistep (BAC_20.rda.0, scope = formula (BAC_20.rda.all), direction = 'both') #stepwise selection
+
+#Summary
+BAC_20.rda.sel.os$anova
+
+#variance partitioning 
+varpart(BAC_20.otu, ~ Temperature_sea, ~ DOC, ~Dissolved_Oxygen_perc, ~NH4, data = BAC_20.env[,c("Temperature_sea", "DOC", "Dissolved_Oxygen_perc", "NH4")])
+
+
+###############################################################
+#Correlation between env. parameters - ONLY FORWARD SELECTED
+###############################################################
+
+#define scale function
+scale_par <- function(x) scale(x, center = FALSE, scale = TRUE)[,1]
+
+#import env.par. data
+metadata.raw <- all.data
+env.par <- c("Temperature_sea", "DOC", "Dissolved_Oxygen_perc", "NH4")
+###env.par <- c("Temperature_sea","Salinity","DOC", "DIN")
+
+#scale all parameters
+metadata.scaled.SRF <- metadata.raw %>% 
+  mutate_at(env.par, scale_par)%>%
+  as.data.frame()
+
+#correlation between the env parameters
+envpar_corr <- metadata.scaled.SRF %>% 
+  select(env.par)%>% 
+  cor_mat(method = "pearson")
+
+#check the p-values
+envpar_corr.pvalues<- envpar_corr %>% cor_get_pval()
+
+#plot
+envpar_corr %>%
+  cor_reorder() %>%
+  pull_lower_triangle() %>%
+  cor_plot(label = TRUE)
+
+
+##########################################################################
+#RDA ordination of the fitted model of bacterial composition data 
+#constrained by environmental variables - ONLY FORWARD SELECTED ENV. VAR
+########################################################################
+
+#subset by fraction and remove NAs in metadata
+phy_obj3
+BAC_20.no.na <- phy_obj3 %>% 
+  subset_samples(
+    !is.na(Temperature_sea) &
+      #!is.na(Salinity) &
+      !is.na(Dissolved_Oxygen_perc) &
+      !is.na(DOC) &
+      #!is.na(TDN) & 
+      #!is.na(Chl_A.1) &
+      #!is.na(NO2) &
+      #!is.na(NO3) &      
+      #!is.na(PO4) &
+      !is.na(NH4) &
+      #!is.na(SiO3) &
+      Dataset == "2020")
+BAC_20.no.na
+
+##remove unobserved OTU and OTU from 2015 dataset
+#BAC_20.no.na <- prune_taxa(taxa_sums(BAC_20.no.na)!=0,BAC_20.no.na)
+#BAC_20.no.na
+
 #extract and scale the env. parameters - only forward selected
 BAC_20.env <- data.frame(sample_data(BAC_20.no.na))[c("Temperature_sea", "DOC", "Dissolved_Oxygen_perc", "NH4")]  
-#BAC_20.env <- data.frame(sample_data(BAC_20.no.na))[c("Temperature_sea","Dissolved_Oxygen_perc","DOC")]  
+###BAC_20.env <- data.frame(sample_data(BAC_20.no.na))[c("Temperature_sea","Dissolved_Oxygen_perc","DOC")]  
 
 BAC_20.env <- as.data.frame(scale(BAC_20.env,center = FALSE, scale = TRUE))
 
@@ -107,27 +195,27 @@ BAC_20.env <- as.data.frame(scale(BAC_20.env,center = FALSE, scale = TRUE))
 BAC_20.otu <- t(otu_table(BAC_20.no.na))
 
 #RDA analysis
-BAC_20.rda.all <- rda (BAC_20.otu ~ ., data = BAC_20.env) # model including all variables 
+BAC_20.rda.all <- rda (BAC_20.otu ~ ., data = BAC_20.env) # model including all variables
 
 #The summary of RDA
 summary(BAC_20.rda.all, display = NULL)
 anova.cca(BAC_20.rda.all, step=1000)
 #anova.cca(BAC_20.rda.all, step=1000, by="axis")
 
+#generate an RDA plot ##########################################
 
-#generate an RDA plot 
+#Extract RDA scores
 BAC_20.rda.scores <- vegan::scores(BAC_20.rda.all,display=c("sp","wa","lc","bp","cn"))
 
-#Locations
+#Make data frame with scores - Locations
 BAC_20.rda.sites <- data.frame(BAC_20.rda.scores$sites)
 BAC_20.rda.sites$SampleID <- as.character(rownames(BAC_20.rda.sites))
 sample_data(BAC_20.no.na)$SampleID <- as.character(rownames(sample_data(BAC_20.no.na)))
 BAC_20.rda.sites <- BAC_20.rda.sites %>%
   left_join(sample_data(BAC_20.no.na))
 
-#Species
+#Make data frame with scores - Species
 BAC_20.rda.species <- data.frame(BAC_20.rda.scores$species)
-BAC_20.rda.species
 BAC_20.rda.species <- BAC_20.rda.species[abs(BAC_20.rda.species$RDA1)>0.25 | abs(BAC_20.rda.species$RDA2)>0.25,]
 BAC_20.rda.species$species_names <- rownames(BAC_20.rda.species)
 TAX <- data.frame(as(tax_table(BAC_20.no.na), "matrix"))
@@ -135,6 +223,7 @@ TAX$species_names <- rownames(TAX)
 BAC_20.rda.species <- BAC_20.rda.species %>%
   left_join(TAX)
 MIC_20.rda.species <- filter(BAC_20.rda.species, Family %in% c(Mic_Ind$Family))
+BAC_20.rda.species <- BAC_20.rda.species[abs(BAC_20.rda.species$RDA1)>0.5 | abs(BAC_20.rda.species$RDA2)>0.5,]
 
 #Draw biplots
 BAC_20.rda.arrows<- BAC_20.rda.scores$biplot*10
@@ -142,9 +231,9 @@ colnames(BAC_20.rda.arrows)<-c("x","y")
 BAC_20.rda.arrows <- as.data.frame(BAC_20.rda.arrows)
 BAC_20.rda.evals <- 100 * (BAC_20.rda.all$CCA$eig / sum(BAC_20.rda.all$CCA$eig))
 
+#Preparation of colour pallete
 colourCount = length(unique(BAC_20.rda.species$Phylum))
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
-
 
 #Plot 
 BAC_20.rda.plot <- ggplot() +
@@ -170,22 +259,8 @@ BAC_20.rda.plot <- ggplot() +
             aes(x, y, label = rownames(BAC_20.rda.arrows)),color="black",alpha=0.5)
 BAC_20.rda.plot
 
-
-#####################################
-#Forward selection of explanatory variables
-#####################################
-
-#based on the tutorial from David Zeleny Lab
-#http://www.davidzeleny.net/anadat-r/doku.php/en:forward_sel
-
-BAC_20.rda.0 <- rda (BAC_20.otu ~ 1, data = BAC_20.env) # model containing only species matrix and intercept
-BAC_20.rda.sel.os <- ordistep (BAC_20.rda.0, scope = formula (BAC_20.rda.all), direction = 'both') #stepwise selection
-
-BAC_20.rda.sel.os$anova
-
-#variance partitioning 
-varpart(BAC_20.otu, ~ Temperature_sea, ~ DOC, ~Dissolved_Oxygen_perc, ~NH4, data = BAC_20.env[,c("Temperature_sea", "DOC", "Dissolved_Oxygen_perc", "NH4")])
-
+###Problem 1: cannot select colours with getPalette
+###Problem 2: some ASVs are not classified at Family level - show the highest level before NA?
 
 
 # CLEAN UP #################################################
